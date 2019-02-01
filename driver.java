209a1a9666b79e596c4ba5d9d5560feeb4635cbe
rleongPd5 <rleong00@stuy.edu>
2019-01-31 23:17:59 -0500

    Added test cases for sqrt from old code

--- driver.java
+++ driver.java
@@ -1,3 +1,28 @@
 public class driver{
-  
+  public static void main(String[] args){
+    //Testing Square Root
+    System.out.println("Testing sqrt\n");
+    System.out.println("Sqrt(1): "+sqrt(1));
+    System.out.println("Math.sqrt(1): "+Math.sqrt(1));
+    System.out.println("Sqrt(2): "+sqrt(2));
+    System.out.println("Math.sqrt(2): "+Math.sqrt(2));
+    System.out.println("Sqrt(10): "+sqrt(10));
+    System.out.println("Math.sqrt(10): "+Math.sqrt(10));
+    System.out.println("Sqrt(25): "+sqrt(25));
+    System.out.println("Math.sqrt(25): "+Math.sqrt(25));
+    System.out.println("Sqrt(36): "+sqrt(36));
+    System.out.println("Math.sqrt(36): "+Math.sqrt(36));
+    System.out.println("Sqrt(0): "+sqrt(0));
+    System.out.println("Math.sqrt(0): "+Math.sqrt(0));
+    System.out.println("Sqrt(1999): "+sqrt(1999));
+    System.out.println("Math.sqrt(1999): "+Math.sqrt(1999));
+    System.out.println("Sqrt(287): "+sqrt(287));
+    System.out.println("Math.sqrt(287): "+Math.sqrt(287));
+    System.out.println("Sqrt(72): "+sqrt(72));
+    System.out.println("Math.sqrt(72): "+Math.sqrt(72));
+    System.out.println("Sqrt(6): "+sqrt(6));
+    System.out.println("Math.sqrt(6): "+Math.sqrt(6));
+
+    
+  }
 }
