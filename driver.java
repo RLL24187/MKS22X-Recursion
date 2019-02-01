42c54a0e828046de5a1d8249950dad69e7c08e04
rleongPd5 <rleong00@stuy.edu>
2019-01-31 23:33:24 -0500

    Fixed test cases to match param types.

--- driver.java
+++ driver.java
@@ -2,27 +2,27 @@ public class driver{
   public static void main(String[] args){
     //Testing Square Root
     System.out.println("Testing sqrt\n");
-    System.out.println("Sqrt(1): "+sqrt(1));
+    System.out.println("Sqrt(1): "+recursion.sqrt(1, 0.001));
     System.out.println("Math.sqrt(1): "+Math.sqrt(1));
-    System.out.println("Sqrt(2): "+sqrt(2));
+    System.out.println("Sqrt(2): "+recursion.sqrt(2, 0.113));
     System.out.println("Math.sqrt(2): "+Math.sqrt(2));
-    System.out.println("Sqrt(10): "+sqrt(10));
+    System.out.println("Sqrt(10): "+recursion.sqrt(10, 0.000002));
     System.out.println("Math.sqrt(10): "+Math.sqrt(10));
-    System.out.println("Sqrt(25): "+sqrt(25));
+    System.out.println("Sqrt(25): "+recursion.sqrt(25, 0.0004));
     System.out.println("Math.sqrt(25): "+Math.sqrt(25));
-    System.out.println("Sqrt(36): "+sqrt(36));
+    System.out.println("Sqrt(36): "+recursion.sqrt(36, 0.025));
     System.out.println("Math.sqrt(36): "+Math.sqrt(36));
-    System.out.println("Sqrt(0): "+sqrt(0));
+    System.out.println("Sqrt(0): "+recursion.sqrt(0, 0.0000000001));
     System.out.println("Math.sqrt(0): "+Math.sqrt(0));
-    System.out.println("Sqrt(1999): "+sqrt(1999));
+    System.out.println("Sqrt(1999): "+recursion.sqrt(1999, 0.00000006));
     System.out.println("Math.sqrt(1999): "+Math.sqrt(1999));
-    System.out.println("Sqrt(287): "+sqrt(287));
+    System.out.println("Sqrt(287): "+recursion.sqrt(287, 0.000078));
     System.out.println("Math.sqrt(287): "+Math.sqrt(287));
-    System.out.println("Sqrt(72): "+sqrt(72));
+    System.out.println("Sqrt(72): "+recursion.sqrt(72, 0.000012));
     System.out.println("Math.sqrt(72): "+Math.sqrt(72));
-    System.out.println("Sqrt(6): "+sqrt(6));
+    System.out.println("Sqrt(6): "+recursion.sqrt(6, 0.00004));
     System.out.println("Math.sqrt(6): "+Math.sqrt(6));
 
-    
+
   }
 }
