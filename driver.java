f7ff21ee76589cfd1ae2f10809a8293c5f45a8f3
rleongPd5 <rleong00@stuy.edu>
2019-02-03 00:13:08 -0500

    Edited print statements for sqrt to include all params

--- driver.java
+++ driver.java
@@ -2,26 +2,26 @@ import java.util.ArrayList;
 public class driver{
   public static void main(String[] args){
     //Testing Square Root
-    System.out.println("Testing sqrt\n");
-    System.out.println("Sqrt(1): "+recursion.sqrt(1, 0.001));
+    System.out.println("Testing sqrt(num, tolerance)\n");
+    System.out.println("Sqrt(1, 0.001): "+recursion.sqrt(1, 0.001));
     System.out.println("Math.sqrt(1): "+Math.sqrt(1));
-    System.out.println("Sqrt(2): "+recursion.sqrt(2, 0.113));
+    System.out.println("Sqrt(2, 0.113): "+recursion.sqrt(2, 0.113));
     System.out.println("Math.sqrt(2): "+Math.sqrt(2));
-    System.out.println("Sqrt(10): "+recursion.sqrt(10, 0.000002));
+    System.out.println("Sqrt(10, 0.000002): "+recursion.sqrt(10, 0.000002));
     System.out.println("Math.sqrt(10): "+Math.sqrt(10));
-    System.out.println("Sqrt(25): "+recursion.sqrt(25, 0.0004));
+    System.out.println("Sqrt(25, 0.0004): "+recursion.sqrt(25, 0.0004));
     System.out.println("Math.sqrt(25): "+Math.sqrt(25));
-    System.out.println("Sqrt(36): "+recursion.sqrt(36, 0.025));
+    System.out.println("Sqrt(36, 0.025): "+recursion.sqrt(36, 0.025));
     System.out.println("Math.sqrt(36): "+Math.sqrt(36));
-    System.out.println("Sqrt(0): "+recursion.sqrt(0, 0.0000000001));
+    System.out.println("Sqrt(0, 0.0000000001): "+recursion.sqrt(0, 0.0000000001));
     System.out.println("Math.sqrt(0): "+Math.sqrt(0));
-    System.out.println("Sqrt(1999): "+recursion.sqrt(1999, 0.00000006));
+    System.out.println("Sqrt(1999, 0.00000006): "+recursion.sqrt(1999, 0.00000006));
     System.out.println("Math.sqrt(1999): "+Math.sqrt(1999));
-    System.out.println("Sqrt(287): "+recursion.sqrt(287, 0.000078));
+    System.out.println("Sqrt(287, 0.000078): "+recursion.sqrt(287, 0.000078));
     System.out.println("Math.sqrt(287): "+Math.sqrt(287));
-    System.out.println("Sqrt(72): "+recursion.sqrt(72, 0.000012));
+    System.out.println("Sqrt(72, 0.000012): "+recursion.sqrt(72, 0.000012));
     System.out.println("Math.sqrt(72): "+Math.sqrt(72));
-    System.out.println("Sqrt(6): "+recursion.sqrt(6, 0.00004));
+    System.out.println("Sqrt(6, 0.00004): "+recursion.sqrt(6, 0.00004));
     System.out.println("Math.sqrt(6): "+Math.sqrt(6));
 
     System.out.println("\n\nTesting fib\n");
