a813bf1dbd6b09c00331c3db22b50b35449c2d12
rleongPd5 <rleong00@stuy.edu>
2019-02-03 22:33:25 -0500

    Copied and edited the test driver: everything works

---
+++ TimDriver.java
@@ -0,0 +1,62 @@
+public class TimDriver{
+  public static void main(String[] args) {
+        System.out.println("sqrt()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("sqrt(4, 0.00001)");
+        System.out.println("EXPECTED: 2");
+        System.out.println(recursion.sqrt(4, 0.00001));         // 2
+
+        System.out.println("----");
+
+        System.out.println("sqrt(8, 0.00001)");
+        System.out.println("EXPECTED: ~2.8284");
+        System.out.println(recursion.sqrt(8, 0.00001));         // ~2.8284
+
+        System.out.println("----");
+
+        System.out.println("sqrt(0, 0.00001)");
+        System.out.println("EXPECTED: 0");
+        System.out.println(recursion.sqrt(0, 0.00001));         // 0
+
+        System.out.println("\n");
+
+        System.out.println("fib()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("fib(0)");
+        System.out.println("EXPECTED: 0");
+        System.out.println(recursion.fib(0));                   // 0
+
+        System.out.println("----");
+
+        System.out.println("fib(1)");
+        System.out.println("EXPECTED: 1");
+        System.out.println(recursion.fib(1));                   // 1
+
+        System.out.println("----");
+
+        System.out.println("fib(7)");
+        System.out.println("EXPECTED: 13");
+        System.out.println(recursion.fib(7));                   // 13
+
+        System.out.println("makeAllSums()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("makeAllSums(0)");
+        System.out.println("EXPECTED: [0]");
+        System.out.println(recursion.makeAllSums(0));           // [0]
+
+        System.out.println("----");
+
+        System.out.println("makeAllSums(1)");
+        System.out.println("EXPECTED: [1, 0]");
+        System.out.println(recursion.makeAllSums(1));           // [1, 0]
+
+        System.out.println("----");
+
+        System.out.println("makeAllSums(3)");
+        System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+        System.out.println(recursion.makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
+  }
+}
